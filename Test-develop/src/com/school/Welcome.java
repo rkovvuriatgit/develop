8468ea1bc5fa59a943d4da9fb4094160ba9ea258
ramu kovvuri <ramueduweb@gmail.com>
2017-08-02 07:50:04 +0530

    init Project creating

---
+++ Test-develop/src/com/school/Welcome.java
@@ -0,0 +1,10 @@
+package com.school;import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+public class Welcome {
+
+	public static String strName="Ramu Kovvuri";
+	
+	public static void main(String args[]) {
+		System.out.println(strName);
+	}
+}
