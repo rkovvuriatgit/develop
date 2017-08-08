59371932297863db4214986cc8e555321e2263d9
ramu kovvuri <ramueduweb@gmail.com>
2017-08-08 09:07:33 +0530

    Signed-off-by: ramu kovvuri <ramueduweb@gmail.com>

--- Test-develop/src/com/school/Welcome.java
+++ Test-develop/src/com/school/Welcome.java
@@ -2,6 +2,7 @@ package com.school;import org.omg.CosNaming.NamingContextExtPackage.StringNameHe
 
 public class Welcome {
 
+	/ramu changes
 	public static String strName="Ramu Kovvuri and Madhuri changes from ramukovvuri4u";
 	
 	public static void main(String args[]) {
